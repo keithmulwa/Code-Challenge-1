3d5be1f84bedf25302171e628117460cc60c5b9e
keithmulwa <150774544+keithmulwa@users.noreply.github.com>
2025-06-08 17:45:54 +0300

    chai.js

---
+++ chaiCalculator/chai.js
@@ -0,0 +1,23 @@
+const user = prompt("Karibu! How many cups of Chai Bora would you like to make?");
+const numberOfCups = Number(user);
+
+function calculateChaiIngredients(numberOfCups) {
+    const water = 200;
+    const milk = 50;
+    const tealeaves = 1;
+    const sugar = 2;
+
+    const totalWater = numberOfCups * water;
+    const totalMilk = numberOfCups * milk;
+    const totalTeaLeaves = numberOfCups * tealeaves;
+    const totalSugar = numberOfCups * sugar;
+
+    console.log("To make " + numberOfCups + " cups of kenyan Chai,you will need:");
+    console.log("Water: " + totalWater + " ml");
+    console.log("Milk: " + totalMilk + " ml");
+    console.log("Tea Leaves (Majani): " + totalTeaLeaves + " tablespoons");
+    console.log("Sugar (Sukari): " + totalSugar + " teaspoons");
+    console.log("");
+    console.log("Enjoy your Chai Bora !");
+}
+
